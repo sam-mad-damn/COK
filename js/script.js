ce3f51367f434508338273a1f2e54d463de33aed
Anastasia Mew <holzunovaa@yandex.ru>
2023-05-04 15:31:14 +0500

    Главная, модальные окна

---
+++ js/script.js
@@ -0,0 +1,23 @@
+function modalWork(btnName,modalName,closeName){
+    function closeModalWindow(wrapper) {
+        wrapper.style.display = 'none';
+    }
+    let modal=document.querySelector(modalName);
+    
+    document.querySelectorAll(btnName).forEach(item => item.addEventListener("click", (e) => {
+        modal.style.display = 'block'
+    }))
+    document.querySelector(closeName).addEventListener('click', () => {
+        closeModalWindow(modal);
+    })
+    modal.addEventListener('click', (e) => {
+        if (e.target == e.currentTarget)
+            closeModalWindow(modal);
+    })
+    document.addEventListener('keydown', (e) => {
+        if (e.code == 'Escape') closeModalWindow(modal);
+    })
+}
+modalWork("#head_info","#modal_wrapper","#close")
+modalWork("#head_word","#words","#close_words")
+modalWork("#doc","#recomends","#close_recomends")
